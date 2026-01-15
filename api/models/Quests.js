797692d764b1e2e187c163f29a998bca31d4b4e5
yohann422 <yohann.fernandez@oclock.school>
2026-01-15 16:32:31 +0100

    Création du fichier index.js dans models

--- api/models/Quests.js
+++ api/models/Quests.js
@@ -4,10 +4,21 @@ import { DataTypes } from "sequelize";
 
 // je crée la table Quests avec les colonnes id, title, description, status
 
-const Quests = sequelize.define('Quests', {
+export const Quests = sequelize.define('Quests', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
+  },
+  title: {
+    type:DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type:DataTypes.TEXT,
+  },
+  status: {
+    type:DataTypes.STRING,
+    defaultValue: "A faire"
   }
 })
--- api/models/index.js
+++ api/models/index.js
@@ -1,1 +1,8 @@
 // si il y a des relations avec les tables le code ici
+// pour centraliser les données aussi
+import { sequelize } from "../config/database.js";
+import { Quests } from "./Quests.js";
+
+export {
+  Quests
+}
